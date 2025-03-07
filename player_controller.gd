3c7673960348ffb44f009eda491f2017ae3cfc22
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-07 17:22:00 -0500

    Improve player controller 2

--- player_controller.gd
+++ player_controller.gd
@@ -2,21 +2,23 @@ extends CharacterBody3D
 
 @export var camera : Camera3D
 @export var mouse_sensitivity = 0.3
+@export var walk_speed = 5
+@export var jump_speed = 5
 var camera_anglev = 0
 
 func _process(delta: float) -> void:
 	
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("strafe_left", "strafe_right", "strafe_forward", "strafe_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	velocity += get_gravity() * delta
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = 5
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
 	
 	if direction:
-		velocity.x = direction.x * 10
-		velocity.z = direction.z * 10
+		velocity.x = direction.x * walk_speed
+		velocity.z = direction.z * walk_speed
 	else:
 		velocity.x = 0
 		velocity.z = 0
