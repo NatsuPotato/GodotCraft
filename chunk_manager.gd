584108710bbe5ae74be60124cbf29c2cfe49139e
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-08 20:24:18 -0500

    Update chunk generation with finite worlds in mind

--- chunk_manager.gd
+++ chunk_manager.gd
@@ -1,21 +1,37 @@
 extends Node3D
 
+# For many decades all GPU's have been able to handle over 2 million polygons
+# which translates to over 512x512 view distance (once you do the basic things
+# like bury algorithm and view frustum culling) it would be INCREDIBLE to play
+# a minecraft with infinite view distance but at this point i suggest focusing
+# on game-play mechanics, difficult and cunning enemies, interesting and
+# inventive objectives, robust and useful interaction mechanics, these are all
+# places where Minecraft falls flat on it's face and they are all places where
+# you can swoop in and succeed as a game developer!
+
+# Basically, infinite worlds are boring actually
+
 const CHUNK_SCENE := preload("res://chunk.tscn")
 
 var generator_threads := []
-var live_chunk_positions := []
+var requested_chunk_positions := []
 
 var noise : FastNoiseLite
 
 func _ready() -> void:
+	
+	for cx in 16:
+		for cy in 16:
+			for cz in 16:
+				requested_chunk_positions.append(Vector3(cx, cy, cz))
 	
 	#https://docs.godotengine.org/en/stable/classes/class_fastnoiselite.html#enum-fastnoiselite-noisetype
 	noise = FastNoiseLite.new()
 	noise.set_seed(RandomNumberGenerator.new().randi())
 	noise.set_domain_warp_frequency(0.1)
 	noise.set_noise_type(FastNoiseLite.TYPE_PERLIN)
-	
-func _process(delta:float) -> void:
+
+func _process(_delta:float) -> void:
 	
 	# wait on terminated threads
 	for i in generator_threads.size():
@@ -29,19 +45,14 @@ func _process(delta:float) -> void:
 			thread.wait_to_finish()
 			generator_threads.remove_at(i)
 	
-	# temp, pick a random position, if it doesn't already have a chunk, spawn a thread
-	if (generator_threads.size() < 16):
+	if (requested_chunk_positions.size() > 0 and generator_threads.size() < 16):
 		
-		var desired_chunk_pos := Vector3i(randi_range(0, 3), randi_range(0, 3), randi_range(0, 3))
+		print(get_child_count() + 1)
 		
-		if live_chunk_positions.find(desired_chunk_pos) == -1:
-			
-			live_chunk_positions.append(desired_chunk_pos)
-			
-			var thread = Thread.new()
-			thread.start(generate_chunk.bind(desired_chunk_pos))
-			generator_threads.append(thread)
-	
+		var thread = Thread.new()
+		thread.start(generate_chunk.bind(requested_chunk_positions.pop_back()))
+		generator_threads.append(thread)
+
 func generate_chunk(chunk_pos:Vector3i):
 	
 	var chunk := CHUNK_SCENE.instantiate()
